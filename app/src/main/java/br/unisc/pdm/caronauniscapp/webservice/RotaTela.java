1e3ef840d2ed820d523e65653503f25a42812f97
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2015-10-14 00:12:12 -0300

    Revert "initial appcarona"
    This reverts commit d8c183a52d3e72a78b1ff8c294ce574af8595e64.

--- app/src/main/java/br/unisc/pdm/caronauniscapp/webservice/RotaTela.java
+++
@@ -1,8 +0,0 @@
-package br.unisc.pdm.caronauniscapp.webservice;
-
-/**
- * Created by Diego on 12/10/2015.
- */
-public interface RotaTela {
-    public void getPositionWs(Double Lat, Double Lng, String locDest);
-}
